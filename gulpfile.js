45fb8814693d074c1ebe4db554b6a088c8bc13d2
hmh <215795583@qq.com>
2017-01-11 19:54:10 +0800

    gulp browserify打包

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,58 @@
-var gulp = require('gulp');
-var webpack = require('webpack');
+var gulp = require('gulp'),
+    clean = require('gulp-clean'),
+    uglify = require('gulp-uglify'),
+    connect = require('gulp-connect'),
+    webpack = require('webpack'),
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream');
 
-var webpack      = require('webpack'),
-    gulp         = require('gulp'),
-    clean        = require('gulp-clean'),
-    uglify       = require('gulp-uglify'),
-    connect      = require('gulp-connect');
-
-gulp.task('clean', function () {
-  return gulp.src('./dist/*', {read: false})
-    .pipe(clean());
+gulp.task('clean', function() {
+    return gulp.src('./dist/*', { read: false })
+        .pipe(clean());
 });
 
-gulp.task('webpack', function (callback) {
-  webpack(require('./webpack.config'),function (error,stats) {
-    if (error){
-      process.stdout.write(String(error));
-      callback();
-      return;
-    }
-    process.stdout.write(String(stats));
-    callback();
-  });
+gulp.task('webpack', function(callback) {
+    webpack(require('./webpack.config'), function(error, stats) {
+        if (error) {
+            process.stdout.write(String(error));
+            callback();
+            return;
+        }
+        process.stdout.write(String(stats));
+        callback();
+    });
+});
+
+gulp.task('browserify', function() {
+    var config = require('./browserify.config');
+    var b = browserify(config);
+    return b.bundle()
+        // Use vinyl-source-stream to make the
+        // stream gulp compatible. Specifiy the
+        // desired output filename here.
+        .pipe(source(config.output.filename))
+        // Specify the output destination
+        .pipe(gulp.dest(config.output.path));
 });
 
 //压缩js文件
-gulp.task('webpack:min:js',['webpack'], function() {
-  return gulp.src(['./dist/*.js'])
-    .pipe(uglify({mangle:{except:['jquery','$','import','module','exports']}}))
-    .pipe(gulp.dest('./dist'));
+gulp.task('min:js', function() {
+    return gulp.src(['./dist/*.js'])
+        .pipe(uglify({ mangle: { except: ['jquery', '$', 'import', 'module', 'exports'] } }))
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('watch', function() {
-  return gulp.watch(['src/**/*.js'], function(event) {
-    if (event.type === 'changed') {
-      gulp.run('dev');
-    }
-  });
+    return gulp.watch(['src/**/*.js'], function(event) {
+        if (event.type === 'changed') {
+            gulp.run('dev');
+        }
+    });
 });
 
-gulp.task('server',function(){
+gulp.task('server', function() {
     connect.server();
 });
 
-gulp.task('dev',['clean','webpack']);
-gulp.task('build',['clean','webpack:min:js']);
+gulp.task('dev', ['clean', 'webpack']);
+gulp.task('build', ['clean', 'webpack', 'min:js']);
